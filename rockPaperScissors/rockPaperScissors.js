d5500efefe29f041f1916382499a06c9e5caf7eb
JohnWebb4 <John.Webb94@gmail.com>
2018-05-01 09:36:57 -0700

    Initial implement rock paper scissors for any size

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,34 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+// Input: number of rounds
+// Output: Every possible combination
+
+// Options: 'rock', 'paper', 'scissors'
+// Array of length 3^n of arrays of n length
+
+
+var rockPaperScissors = function (numRounds) {
+  var possibleChoices = ['rock', 'paper', 'scissors'];
+  var roundIndexChoices = Array(numRounds).fill(0);
+  var arrayRounds = [];
+
+  var numChoices = 3;
+  var numCombinations = numChoices ** numRounds;
+
+  for (var combinationIndex = 0; combinationIndex < numCombinations; combinationIndex++) {
+    var roundChoices = roundIndexChoices.map(function(choiceIndex) {
+      return possibleChoices[choiceIndex];
+    });
+
+    arrayRounds.push(roundChoices);
+
+    roundIndexChoices = incrementAbacusArray(roundIndexChoices, numChoices);
+  }
+
+  return arrayRounds;
+};
+
+var incrementAbacusArray = function(array, maxValue) {
+
 };
