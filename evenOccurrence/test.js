8870902b9429417d725ed497ec0e2ed9cee28958
JohnWebb4 <John.Webb94@gmail.com>
2018-05-02 11:59:37 -0700

    Test even occurrence with mixed array

--- evenOccurrence/test.js
+++ evenOccurrence/test.js
@@ -15,4 +15,18 @@ describe('evenOccurrence', function() {
   it('should handle random order', function() {
     expect(evenOccurrence([2, 1, 3, 2, 57])).to.equal(2);
   });
+
+  // Spectator tests
+  it('should return the first even occurrence of an array of numbers', function() {
+    expect(evenOccurrence([1, 3, 3, 3, 2, 4, 4, 2, 5])).to.equal(2);
+  });
+
+  it('should return the first even occurrence of a mixed array', function() {
+    expect(evenOccurrence(['meow', 1, 1, 'meow'])).to.equal('meow');
+  });
+
+  it('should return the first even occurrence in an array with multiple even occurrences', function() {
+    var array = ['doublerainbow', 'grumpycat', 'grumpycat', 'doublerainbow'];
+    expect(evenOccurrence(array)).to.equal('doublerainbow');
+  });
 });
