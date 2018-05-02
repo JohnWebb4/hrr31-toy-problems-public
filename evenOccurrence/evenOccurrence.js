d2c0a99c6610250b4807db306da3e2e045b07c96
JohnWebb4 <John.Webb94@gmail.com>
2018-05-02 09:24:59 -0700

    Whiteboard even occurance

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -1,6 +1,6 @@
 /*
  * Find the first item that occurs an even number of times in an array.
- * Remember to handle multiple even-occurrence items and return the first one. 
+ * Remember to handle multiple even-occurrence items and return the first one.
  * Return null if there are no even-occurrence items.
 */
 
@@ -9,6 +9,19 @@
  * var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
  * console.log(onlyEven); //  4
 */
+
+// Input: Array
+// Output: First number to occur even number of times
+
+// Iterate through array
+// Store first index of number
+// Store number of occurances of number
+// Iterate through number results
+// Store value of number with lowest index and occurs an even number of times
+// Return value
+
+// Reduce array to object
+// Key number value and store another object with lowest index and if occured an even number of times
 
 var evenOccurrence = function(arr) {
   // Your code here.
