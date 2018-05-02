e355f8bd4e39fee3c6e0fabd81678ce05e804c84
JohnWebb4 <John.Webb94@gmail.com>
2018-05-02 09:56:49 -0700

    Write pass or fail with test

--- evenOccurrence/test.js
+++ evenOccurrence/test.js
@@ -10,10 +10,15 @@ testEvenOccuranceEquals([1, 2, 3], null, 'Should handle no even occurances');
 testEvenOccuranceEquals([1, 2, 1, 3], 1, 'Should return even occurance (1)');
 
 // Should handle multiple even occurances
-testEvenOccuranceEquals([1, 2, 1, 2, 3, 3], 'Should return first even occurance');
+testEvenOccuranceEquals([1, 2, 1, 2, 3, 3], 1, 'Should return first even occurance');
 
-function testEvenOccuranceEquals(arr, value, errorMessage) {
-  if (evenOccurrence(arr) !== value) {
-    console.log(errorMessage);
+// Should handle random assortment
+testEvenOccuranceEquals([2, 1, 3, 2, 57], 2, 'Should handle random assortment');
+
+function testEvenOccuranceEquals(arr, value, testMessage) {
+  if (evenOccurrence(arr) === value) {
+    console.log('PASSED: ' + testMessage);
+  } else {
+    console.log('FAILED: ' + testMessage);
   }
 }
