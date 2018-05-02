d9d9dd45d17374427140e088cf04796550b55818
JohnWebb4 <John.Webb94@gmail.com>
2018-05-02 11:10:12 -0700

    Refactor even occurrence tests to use Mocha

--- evenOccurrence/test.js
+++ evenOccurrence/test.js
@@ -1,24 +1,18 @@
-// Tests for occurances
+describe('evenOccurrence', function() {
 
-// Should return null for empty array
-testEvenOccuranceEquals([], null, 'Should handle empty array');
+  it('should handle empty array', function() {
+    expect(evenOccurrence([])).to.equal(null);
+  });
 
-// Should return null if no even occurances
-testEvenOccuranceEquals([1, 2, 3], null, 'Should handle no even occurances');
+  it('should return even occurrence 1', function() {
+    expect(evenOccurrence([1, 2, 1, 3])).to.equal(1);
+  });
 
-// Should return even occurance
-testEvenOccuranceEquals([1, 2, 1, 3], 1, 'Should return even occurance (1)');
+  it('should return first even occurrence 1', function() {
+    expect(evenOccurrence([1, 2, 1, 2, 3, 3])).to.equal(1);
+  });
 
-// Should handle multiple even occurances
-testEvenOccuranceEquals([1, 2, 1, 2, 3, 3], 1, 'Should return first even occurance');
-
-// Should handle random assortment
-testEvenOccuranceEquals([2, 1, 3, 2, 57], 2, 'Should handle random assortment');
-
-function testEvenOccuranceEquals(arr, value, testMessage) {
-  if (evenOccurrence(arr) === value) {
-    console.log('PASSED: ' + testMessage);
-  } else {
-    console.log('FAILED: ' + testMessage);
-  }
-}
+  it('should handle random order', function() {
+    expect(evenOccurrence([2, 1, 3, 2, 57])).to.equal(2);
+  });
+});
