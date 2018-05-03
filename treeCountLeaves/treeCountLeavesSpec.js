0aec2c596fe9e23fc3bf47643f98b9f49d0174c5
JohnWebb4 <John.Webb94@gmail.com>
2018-05-03 09:29:26 -0700

    Test count direct leaves

--- treeCountLeaves/treeCountLeavesSpec.js
+++ treeCountLeaves/treeCountLeavesSpec.js
@@ -14,23 +14,4 @@ describe('treeCountLeaves', function() {
 
     expect(tree.countLeaves()).to.equal(3);
   });
-
-  it('should work for indirect leaves', function() {
-    tree.addChild(1);
-    tree.children[0].addChild(2);
-    tree.children[0].addChild(3);
-
-    expect(tree.countLeaves()).to.equal(2);
-  });
-
-  it('should work for direct and indirect leaves', function() {
-    tree.addChild(1);
-    tree.addChild(2);
-    tree.addChild(3);
-    tree.children[1].addChild(4);
-    tree.children[2].addChild(5);
-    tree.children[2].children[0].addChild(6);
-
-    expect(tree.countLeaves()).to.equal(3);
-  });
 });
