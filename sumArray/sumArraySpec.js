c137a61755e4d7a7b1fb5f03f70e233bf0879679
JohnWebb4 <John.Webb94@gmail.com>
2018-05-10 15:17:06 -0700

    Test summing array is a function and does not mutate input

---
+++ sumArray/sumArraySpec.js
@@ -0,0 +1,29 @@
+describe('sumArray', () => {
+  var tests;
+  var results;
+
+  beforeEach(() => {
+    tests = [
+      [1, 2, 3], // => 6
+      [1, 2, 3, -4], // 6
+      [1, 2, 3, -4, 5], // 7
+      [4, -1, 5], // => 8
+      [10, -11, 11] // 11
+    ];
+
+    results = [
+      6, 6, 7, 8, 11
+    ];
+
+  });
+
+  it('should be a function', () => {
+    expect(sumArray).to.be.a('function');
+  });
+
+  it('should not mutate the input array', () => {
+    var aTest = tests[0].slice();
+    sumArray(aTest);
+    expect(aTest).to.eql(tests[0]);
+  });
+});
