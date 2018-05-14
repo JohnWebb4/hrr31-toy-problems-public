d9390785fc1dda5aa860d93084c94d5e7c3b6633
JohnWebb4 <John.Webb94@gmail.com>
2018-05-13 18:40:51 -0700

    Test tree mapInPlace is a function

--- treeMap/treeMapSpec.js
+++ treeMap/treeMapSpec.js
@@ -41,3 +41,35 @@ describe('treeMap', () => {
     expect(tree.map(multiplyByTwo)).to.eql(resultTree);
   });
 });
+
+describe('treeMapInPlace', () => {
+  var tree;
+  var multiplyByTwo;
+
+  beforeEach(() => {
+    tree = new Tree(1);
+    tree.addChild(2);
+    tree.addChild(3);
+    tree.children[0].addChild(4);
+    tree.children[0].addChild(5);
+    tree.children[1].addChild(6);
+    tree.children[1].addChild(7);
+
+    multiplyByTwo = function (value) {
+       return value * 2;
+    };
+
+    resultTree = new Tree(2);
+    resultTree.addChild(4);
+    resultTree.addChild(6);
+    resultTree.children[0].addChild(8);
+    resultTree.children[0].addChild(10);
+    resultTree.children[1].addChild(12);
+    resultTree.children[1].addChild(14);
+  });
+
+
+  it('should be a function', () => {
+    expect(tree.mapInPlace).to.be.a('function');
+  });
+});
