7132ab950ffbc3e59f5aa1db7b9954fb716376a7
JohnWebb4 <John.Webb94@gmail.com>
2018-05-13 19:12:07 -0700

    Test telephone numbers returns an array

--- telephoneWords/telephoneWordsSpec.js
+++ telephoneWords/telephoneWordsSpec.js
@@ -1,5 +1,27 @@
 describe('telephoneWords', () => {
+  var phoneNumber;
+  var numberPossibilities;
+
+  beforeEach(() => {
+    phoneNumber = '0123';
+    possiblePhoneWords = [
+      '01AD',
+      '01AE',
+      '01AF',
+      '01BD',
+      '01BE',
+      '01BF',
+      '01CD',
+      '01CE',
+      '01CF'
+    ];
+  });
+
   it('should be a function', () => {
     expect(telephoneWords).to.be.a('function');
+  });
+
+  it('should return an array', () => {
+    expect(Array.isArray(telephoneWords(phoneNumber))).to.be.true;
   });
 });
