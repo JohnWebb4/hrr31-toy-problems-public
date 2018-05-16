3c2ca020e9d8a5acbd91fe1a52b1c3582b302c32
JohnWebb4 <John.Webb94@gmail.com>
2018-05-16 09:36:42 -0700

    Test returns a number

--- coinSums/coinSumsSpec.js
+++ coinSums/coinSumsSpec.js
@@ -1,5 +1,20 @@
 describe('coinSum', function() {
+  var changeTotals;
+
+  beforeEach(function() {
+    changeTotals = [
+      0, 1, 2, 3,
+      10, 20, 30,
+      100, 200, 300,
+      1000, 2000, 3000
+    ];
+  });
+
   it('should be a function', function() {
     expect(makeChange).to.be.a('function');
+  });
+
+  it('should return a number', function() {
+    expect(makeChange(changeTotals[0])).to.be.a('number');
   });
 });
