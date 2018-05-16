a20a10c97e6f4dd3705b4a52e014b905cf4e71c0
JohnWebb4 <John.Webb94@gmail.com>
2018-05-16 09:40:23 -0700

    Test coinSums gets coin combinations for a total

--- coinSums/coinSumsSpec.js
+++ coinSums/coinSumsSpec.js
@@ -3,10 +3,10 @@ describe('coinSum', function() {
 
   beforeEach(function() {
     changeTotals = [
-      0, 1, 2, 3,
-      10, 20, 30,
-      100, 200, 300,
-      1000, 2000, 3000
+      0, 1, 2, 3
+    ];
+    changeResults = [
+      0, 1, 2, 2
     ];
   });
 
@@ -16,5 +16,11 @@ describe('coinSum', function() {
 
   it('should return a number', function() {
     expect(makeChange(changeTotals[0])).to.be.a('number');
+  });
+
+  it('should get total number of combinations for change', function() {
+    changeTotals.forEach(function(total, index) {
+      expect(makeChange(total)).to.equal(changeResults[index]);
+    });
   });
 });
