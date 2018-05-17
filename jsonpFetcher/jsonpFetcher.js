249b9ae79fe6b8d11603b27ea96105ed10a68016
JohnWebb4 <John.Webb94@gmail.com>
2018-05-17 10:10:12 -0700

    Get JSONP response and pass to callback

--- jsonpFetcher/jsonpFetcher.js
+++ jsonpFetcher/jsonpFetcher.js
@@ -28,7 +28,59 @@
  * Feel free to use Google in searching for your ideal implementation!
  */
 
+// Whiteboard
+// What is JSONP? Json with padding.
+// How do I initiate a jsonp request?
+//// Can't use Jquery
+//// Can't use XMLHttpRequest
+//// Need to add script tag to body.
+////// Set script source to url
+////// Supply callback ?callback=...
 
+// Callback Structure
+// O: None.
+// I: jsonp data
+// C: None.
+// E: No data.
+
+// JsonpRequest Structure
+// O: None. Runs callback on request complete
+// I: Url to call, and callback function to run on complete
+// C: None
+// E:
+
+// Breakdown
+// Create script tag in document and store DOM object
+// Set script source with query callback set to callback
+// Append script tag to document
 
 var jsonpRequest = function(url, callback) {
+  var script = document.createElement('script');
+
+  script.src = url + '?q=serverCallback';
+
+  document.getElementsByTagName('head')[0].appendChild(script);
 };
+
+var serverCallback = function(res, callback) {
+  var body = '';
+
+  res.on('data', function(chunk) {
+    body += chunk;
+  });
+
+  res.on('end', function() {
+    var json = JSON.parse(body);
+    callback(json);
+  });
+};
+
+// jsonpRequest('http://toy-problems.hackreactor.com/jsonparty', function (data) {
+//   console.log(data.response); // "Aw yeah, now we're JSONPartying"
+//   console.log(data.random); // 3558
+// });
+
+// // Subsequent requests should have properly random responses:
+// jsonpRequest('http://toy-problems.hackreactor.com/jsonparty', function (data) {
+//   console.log(data.random); // 1733
+// });
