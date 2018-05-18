7cb7214f8360f7c09bbf83e8f9fd9128113dee68
JohnWebb4 <John.Webb94@gmail.com>
2018-05-18 10:15:38 -0700

    Create event system for object

--- eventingLibrary/eventingLibrary.js
+++ eventingLibrary/eventingLibrary.js
@@ -20,7 +20,64 @@
  * - It is not necessary to write a way to remove listeners.
  */
 
+ // Whiteboard
+ // I: Object to extend
+ // O: Input object extended with on and trigger
+ // C: No space/time complexity. See Caveats.
+ // E: Multiple listeners for same event.
+
+ // Breakdown
+ // Declare events object in obj to hold all events
+ // Declare on function in obj
+ // Declare trigger event in obj
+ // Return obj
+
+
+ // On functon
+ // I: event name, and callback (accepts no arguments)
+ // O: None
+ // C: None
+ // E: Adding multiple listeners to same callback
+
+ // Breakdown on
+ //// If events does not exit, create empty object
+ //// If event name exists as ken in events, push callback
+ //// Else create array with callback as first element
+
+ // Trigger event
+ // I: event name
+ // O: None
+ // C: None
+ // E: Call all listeners for callback
+
+ // Breakdown trigger
+ //// If events doesn't exist return
+ //// If event name does not exit in events return
+ //// Get array at event name
+ //// Iterate through array and call listener callback
+
 var mixEvents = function(obj) {
-  // TODO: Your code here
+  obj.events = {};
+
+  obj.on = function(eventName, callback) {
+    this.events = this.events || {};
+    this.events[eventName] = this.events.eventName || [];
+    this.events[eventName].push(callback);
+  };
+
+  obj.trigger = function(eventName) {
+    if (!this.events) {
+      return;
+    }
+
+    if (!this.events[eventName]) {
+      return;
+    }
+
+    this.events[eventName].forEach(function(callback) {
+      callback();
+    });
+  }
+
   return obj;
 };
