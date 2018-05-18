bb7628c098b4403c9bdd32c0edee85345398f056
JohnWebb4 <John.Webb94@gmail.com>
2018-05-18 10:11:15 -0700

    Test creates events holder and an event

--- eventingLibrary/eventingLibrarySpec.js
+++ eventingLibrary/eventingLibrarySpec.js
@@ -1,5 +1,27 @@
 describe('eventingLibrary / mixEvents', function() {
+  var obj;
+  var mixObj;
+
+  beforeEach(function() {
+    obj = {
+      name: 'John',
+      accoundId: 1
+    };
+
+    mixObj = mixEvents(obj);
+  });
+
   it('should be a function', function() {
     expect(mixEvents).to.be.a('function');
+  });
+
+  it('should have events object', function() {
+    expect(mixObj.events).to.be.an('object');
+  });
+
+  it('should register event', function() {
+    mixObj.on('deposit', function() {conosle.log('hi');} );
+    console.log('Events', mixObj.events);
+    expect(mixObj.events.deposit).to.be.an('array');
   });
 });
