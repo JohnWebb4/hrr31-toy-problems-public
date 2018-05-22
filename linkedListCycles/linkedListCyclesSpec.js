b478ebb8c397c472d914ed2c8ca1669e0584a789
JohnWebb4 <John.Webb94@gmail.com>
2018-05-22 09:53:30 -0700

    Test has cycles returns a boolean

--- linkedListCycles/linkedListCyclesSpec.js
+++ linkedListCycles/linkedListCyclesSpec.js
@@ -1,5 +1,27 @@
 describe('linkedListCycles', function() {
+  var linearList;
+  var cyclicList;
+
+  beforeEach(function() {
+    linearList = Node('A');
+    var nodeB = linearList.next = Node('B');
+    var nodeC = nodeB.next = Node('C');
+    var nodeD = nodeC.next = Node('D');
+    var nodeE = nodeD.next = Node('E');
+
+    cyclicList = Node('A');
+    var cNodeB = cyclicList.next = Node('B');
+    var cNodeC = cNodeB.next = Node('C');
+    var cNodeD = cNodeC.next = Node('D');
+    var cNodeE = cNodeD.next = Node('E');
+    cNodeE.next = cNodeB;
+  });
+
   it('should be a function', function() {
     expect(hasCycle).to.be.a('function');
+  });
+
+  it('should return a Boolean', function() {
+    expect(hasCycle(linearList)).to.be.a('boolean');
   });
 });
