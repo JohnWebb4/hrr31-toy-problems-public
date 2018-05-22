035b37865fd04ea0946df0c03f429cb2fd849387
JohnWebb4 <John.Webb94@gmail.com>
2018-05-22 15:48:39 -0700

    Test hash table resizes

--- hashTableResizing/hashTableResizingSpec.js
+++ hashTableResizing/hashTableResizingSpec.js
@@ -2,4 +2,38 @@ describe('hashTableResizing', function() {
   it('should be a function', function() {
     expect(makeHashTable).to.be.a('function');
   });
+
+  it('should resize', function() {
+    // if your hashtable isn't resizing, its going to start running more
+    // and more slowly with a large number of inserts and retrievals.
+    var hashTable = makeHashTable();
+    var n = 10;
+    var id = 0;
+    var diffs = [];
+    var endTime;
+    var startTime;
+    for (var i = 0; i <= n; i++) {
+      startTime = new Date();
+      for (var j = 0; j < 1000; j++) {
+        hashTable.insert('userid:' + id++, 'Syd Mead');
+      }
+      for (j = 0; j < 100; j++) {
+        hashTable.retrieve('userid:' + Math.floor(Math.random() * i));
+      }
+      endTime = new Date();
+      diffs.push(endTime - startTime);
+    }
+    var sum = function sum(arr) {
+      var total = 0;
+      for (var i = 0; i < arr.length; i++) {
+        total += arr[i];
+      }
+      return total;
+    };
+    // we should expect the first iteration to take up ruffly 1 / n
+    // of the total time. we give it some wiggle room by letting it be as
+    // low as a 1 / (n*2) of the total duration.
+    var ratio = diffs[0] / sum(diffs);
+    expect(ratio).to.be.above(1 / (n * 2));
+  });
 });
