7e073bc39c210b38cf154705363caeff89c25a20
JohnWebb4 <John.Webb94@gmail.com>
2018-05-23 09:29:26 -0700

    Test getting anagrams returns an array

--- allAnagrams/allAnagramsSpec.js
+++ allAnagrams/allAnagramsSpec.js
@@ -1,5 +1,17 @@
 describe('allAnagrams', function() {
+  var string;
+  var resultAnagrams;
+
+  beforeEach(function() {
+    string = 'abc';
+    var resultAnagrams = [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ];
+  });
+
   it('should be a function', function() {
     expect(allAnagrams).to.be.a('function');
+  });
+
+  it('should return an array', function() {
+    expect(allAnagrams(string)).to.be.an('array');
   });
 });
