df4ae38b4f35b945ad7cbbdc58d5623fea3a73a7
JohnWebb4 <John.Webb94@gmail.com>
2018-05-22 19:39:10 -0700

    Test double digit numbers

--- numberToEnglish/numberToEnglishSpec.js
+++ numberToEnglish/numberToEnglishSpec.js
@@ -15,4 +15,21 @@ xdescribe('numberToEnglish', () => {
     expect(8 .toEnglish()).to.equal('eight');
     expect(9 .toEnglish()).to.equal('nine');
   });
+
+  it('should write tens', () => {
+    expect(10 .toEnglish()).to.equal('ten');
+    expect(20 .toEnglish()).to.equal('twenty');
+    expect(30 .toEnglish()).to.equal('thirty');
+    expect(40 .toEnglish()).to.equal('forty');
+    expect(50 .toEnglish()).to.equal('fifty');
+    expect(60 .toEnglish()).to.equal('sixty');
+    expect(70 .toEnglish()).to.equal('seventy');
+    expect(80 .toEnglish()).to.equal('eighty');
+    expect(90 .toEnglish()).to.equal('ninety');
+
+    // compounds from 21-99 should be hyphenated
+    expect(44 .toEnglish()).to.equal('forty-four');
+    expect(67 .toEnglish()).to.equal('sixty-seven');
+    expect(99 .toEnglish()).to.equal('ninety-nine');
+  });
 });
