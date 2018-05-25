20ef1f98802aefd62ee34f842eef326108110967
JohnWebb4 <John.Webb94@gmail.com>
2018-05-25 10:26:01 -0700

    Test longest palindrome returns a string

--- longestPalindrome/longestPalindromeSpec.js
+++ longestPalindrome/longestPalindromeSpec.js
@@ -1,5 +1,15 @@
 describe('longestPalindrome', function() {
+  var sentence;
+
+  beforeEach(function() {
+    sentence = 'thissi a palindrome'
+  });
+
   it('should be a function', function() {
     expect(longestPalindrome).to.be.a('function');
+  });
+
+  it('should return a string', function() {
+    expect(longestPalindrome(sentence)).to.be.a('string');
   });
 });
