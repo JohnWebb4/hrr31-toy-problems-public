0a9a75c8d778c598b8d78c03e9481fb12dcc0826
JohnWebb4 <John.Webb94@gmail.com>
2018-05-29 09:51:46 -0700

    Test multiply returns a number

--- implementingOperators/implementingOperatorsSpec.js
+++ implementingOperators/implementingOperatorsSpec.js
@@ -2,4 +2,8 @@ describe('implementingOperators', () => {
   it('multiple should be a function', () => {
     expect(multiply).to.be.a('function');
   });
+
+  it('should return a number', () => {
+    expect(multiply(1, 1)).to.be.a('number');
+  });
 });
