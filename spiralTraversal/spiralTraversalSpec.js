dd82e39980163262c0bb5aa7f10b8e2cc960cbb6
JohnWebb4 <John.Webb94@gmail.com>
2018-05-30 09:37:14 -0700

    Test spiral traversal returns an array

--- spiralTraversal/spiralTraversalSpec.js
+++ spiralTraversal/spiralTraversalSpec.js
@@ -1,5 +1,22 @@
 describe('spiralTraversal', () => {
+  let arrayToTraverse;
+  let spiralArray;
+
+  beforeEach(() => {
+    arrayToTraverse = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ];
+
+    spiralArray = [1, 2, 3, 6, 9, 8, 7, 4, 5];
+  });
+
   it('should be a function', () => {
     expect(spiralTraversal).to.be.a('function');
+  });
+
+  it('should return an array', () => {
+    expect(spiralTraversal(arrayToTraverse)).to.be.an('array');
   });
 });
