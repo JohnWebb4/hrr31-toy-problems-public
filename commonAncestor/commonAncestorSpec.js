ac1d0be2677100c14a0808e944cd560714678fad
JohnWebb4 <John.Webb94@gmail.com>
2018-06-01 09:39:54 -0700

    Test common ancestor returns a boolean

--- commonAncestor/commonAncestorSpec.js
+++ commonAncestor/commonAncestorSpec.js
@@ -1,5 +1,24 @@
 describe('commonAncestor', () => {
+  let grandma;
+  let mom;
+  let me;
+  let potato;
+
+  beforeEach(() => {
+    grandma = new Tree();
+    mom = new Tree();
+    me = new Tree();
+    potato = new Tree();
+
+    grandma.addChild(mom);
+    mom.addChild(me);
+  });
+
   it('should be a function', () => {
-    expect(Tree.prototype.getClosestCommonAncestor).to.be.a('function');
+    expect(grandma.getClosestCommonAncestor).to.be.a('function');
+  });
+
+  it('should return a boolean', () => {
+    expect(grandma.getClosestCommonAncestor(mom)).to.be.a('boolean');
   });
 });
