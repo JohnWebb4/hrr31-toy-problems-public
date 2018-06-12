76956cded3badbaaf7a54cca747c126f9a2b301c
JohnWebb4 <John.Webb94@gmail.com>
2018-06-12 09:46:25 -0700

    Test lru cache has get function

--- lruCache/lruCacheSpec.js
+++ lruCache/lruCacheSpec.js
@@ -12,4 +12,8 @@ describe('lruCache', () => {
   it('should get size', () => {
     expect(lruCache.size()).to.equal(0);
   });
+
+  it('should have a get function', () => {
+    expect(lruCache.get).to.be.a('function');
+  });
 });
