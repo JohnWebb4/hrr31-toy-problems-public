15c9a03d9135266682e97a0fc3c199dbb46dd050
JohnWebb4 <John.Webb94@gmail.com>
2018-06-13 09:36:56 -0700

    Test returns a number

--- romanNumeralTranslator/romanNumeralTranslatorSpec.js
+++ romanNumeralTranslator/romanNumeralTranslatorSpec.js
@@ -2,4 +2,8 @@ describe('romanNumeralTranslator', () => {
   it('should be a function', () => {
     expect(translateRomanNumeral).to.be.a('function');
   });
+
+  it('should return a number', () => {
+    expect(translateRomanNumeral('I')).to.be.a('number');
+  });
 });
