0f1ffc14a44d5bf725f3c050d72f21eb7154492a
JohnWebb4 <John.Webb94@gmail.com>
2018-06-13 10:04:29 -0700

    Test converting numerals and subtractive notation

--- romanNumeralTranslator/romanNumeralTranslatorSpec.js
+++ romanNumeralTranslator/romanNumeralTranslatorSpec.js
@@ -6,4 +6,20 @@ describe('romanNumeralTranslator', () => {
   it('should return a number', () => {
     expect(translateRomanNumeral('I')).to.be.a('number');
   });
+
+  it('should convert roman numerals', () => {
+    expect(translateRomanNumeral('I')).to.equal(1);
+    expect(translateRomanNumeral('VII')).to.equal(7);
+    expect(translateRomanNumeral('XXVI')).to.equal(26);
+    expect(translateRomanNumeral('LX')).to.equal(60);
+    expect(translateRomanNumeral('CL')).to.equal(150);
+    expect(translateRomanNumeral('DCCC')).to.equal(800);
+    expect(translateRomanNumeral('MD')).to.equal(1500);
+    expect(translateRomanNumeral('MMXVIII')).to.equal(2018);
+  });
+
+  it('should handle subtractive numerals', () => {
+    expect(translateRomanNumeral('IX')).to.equal(9);
+    expect(translateRomanNumeral('CIV')).to.equal(104);
+  });
 });
