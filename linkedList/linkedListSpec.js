fc0f8dd65b1822f07aac1263a5ac011abaf6bc0e
JohnWebb4 <John.Webb94@gmail.com>
2018-06-15 09:55:10 -0700

    Test add to tail returns value

--- linkedList/linkedListSpec.js
+++ linkedList/linkedListSpec.js
@@ -10,6 +10,9 @@ describe('linkedList', () => {
     expect(linkedList.addToTail).to.be.a('function');
     expect(linkedList.removeHead).to.be.a('function');
     expect(linkedList.contains).to.be.a('function');
-    expect(linkedList.makeNode).to.be.a('function');
+  });
+
+  it('should return item added to tail', () => {
+    expect(linkedList.addToTail(1)).to.equal(1);
   });
 });
