890462060983486a704bd280a3a4fc297c7d6645
JohnWebb4 <John.Webb94@gmail.com>
2018-06-14 20:55:35 -0700

    Create stack

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -1,5 +1,5 @@
 /**
- * Write a stack using your preferred instantiation pattern. 
+ * Write a stack using your preferred instantiation pattern.
  * Avoid using native array methods i.e., push, pop, and length.
  * Once you're done, implement a queue using two stacks.
  */
@@ -7,41 +7,52 @@
 /**
   * Stack Class
   */
-var Stack = function() {
+const Stack = function Stack() {
+  let length = 0;
 
   // add an item to the top of the stack
-  this.push = function() {
+  this.push = function push(value) {
+    this[this.size] = value;
+    length += 1;
+    return this[this.size];
   };
 
   // remove an item from the top of the stack
-  this.pop = function() {
+  this.pop = function pop() {
+    if (length === 0) {
+      return null;
+    }
+
+    length -= 1;
+    return this[this.size];
   };
 
   // return the number of items in the stack
-  this.size = function() {
+  this.size = function size() {
+    return length;
   };
 };
 
 /**
   * Queue Class
   */
-var Queue = function() {
+const Queue = function Queue() {
   // Use two `stack` instances to implement your `queue` Class
   var inbox = new Stack();
   var outbox = new Stack();
 
   // called to add an item to the `queue`
-  this.enqueue = function() {
+  this.enqueue = function enqueue() {
     // TODO: implement `enqueue`
   };
 
   // called to remove an item from the `queue`
-  this.dequeue = function() {
+  this.dequeue = function dequeue() {
     // TODO: implement `dequeue`
   };
 
   // should return the number of items in the queue
-  this.size = function() {
+  this.size = function size() {
     // TODO: implement `size`
   };
 };
