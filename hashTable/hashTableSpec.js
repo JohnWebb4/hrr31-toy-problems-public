e859a0403ae194f9416853199ddb56a723e62aab
JohnWebb4 <John.Webb94@gmail.com>
2018-06-14 23:32:34 -0700

    Test hash table has insert, retrieve, and remove

--- hashTable/hashTableSpec.js
+++ hashTable/hashTableSpec.js
@@ -2,4 +2,12 @@ describe('hashTable', () => {
   it('should have makeHashTable', () => {
     expect(makeHashTable).to.be.a('function');
   });
+
+  it('should make a hash table with insert, retrieve, and remove', () => {
+    const hashTable = makeHashTable();
+    expect(hashTable).to.be.an('object');
+    expect(hashTable.insert).to.be.a('function');
+    expect(hashTable.retrieve).to.be.a('function');
+    expect(hashTable.remove).to.be.a('function');
+  });
 });
