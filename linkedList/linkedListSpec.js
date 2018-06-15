019ef75a5ad50214536f587e554cbcd500ea92e3
JohnWebb4 <John.Webb94@gmail.com>
2018-06-15 09:25:50 -0700

    Test has a psuedoclassical linked list

--- linkedList/linkedListSpec.js
+++ linkedList/linkedListSpec.js
@@ -1,5 +1,15 @@
 describe('linkedList', () => {
-  it('should pass', () => {
-    return true;
+  let linkedList;
+
+  beforeEach(() => {
+    linkedList = new LinkedList();
+  });
+
+  it('should be a psuedoclassical linked list', () => {
+    expect(linkedList).to.be.an('object');
+    expect(linkedList.addToTail).to.be.a('function');
+    expect(linkedList.removeHead).to.be.a('function');
+    expect(linkedList.contains).to.be.a('function');
+    expect(linkedList.makeNode).to.be.a('function');
   });
 });
