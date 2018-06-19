cc9ab26c77e761cfb5fcd1536c690b30d78415fd
JohnWebb4 <John.Webb94@gmail.com>
2018-06-19 09:46:04 -0700

    Test returns an array of array of strings

--- rockPaperScissors/rockPaperScissorsSpec.js
+++ rockPaperScissors/rockPaperScissorsSpec.js
@@ -2,4 +2,11 @@ describe('rockPaperScissors', () => {
   it('should be a function', () => {
     expect(rockPaperScissors).to.be.a('function');
   });
+
+  it('should return array of arrays of strings', () => {
+    const rpsArray = rockPaperScissors(3);
+    expect(rpsArray).to.be.an('array');
+    expect(rpsArray[0]).to.be.an('array');
+    expect(rpsArray[0][0]).to.be.a('string');
+  });
 });
