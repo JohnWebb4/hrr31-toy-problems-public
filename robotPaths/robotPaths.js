6703f8c43e51cc7c5668f469b41f3313abb04ef5
JohnWebb4 <John.Webb94@gmail.com>
2018-06-19 09:43:51 -0700

    Lint robot paths and outline

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -1,33 +1,57 @@
 /**
- *  
- *  A robot located at the top left corner of a 5x5 grid is trying to reach the 
- *  bottom right corner. The robot can move either up, down, left, or right, 
- *  but cannot visit the same spot twice. How many possible unique paths are 
- *  there to the bottom right corner? 
- *  
+ *
+ *  A robot located at the top left corner of a 5x5 grid is trying to reach the
+ *  bottom right corner. The robot can move either up, down, left, or right,
+ *  but cannot visit the same spot twice. How many possible unique paths are
+ *  there to the bottom right corner?
+ *
  *  make your solution work for a grid of any size.
  *
  */
 
 // A Board class will be useful
 
-var makeBoard = function(n) {
-  var board = [];
-  for (var i = 0; i < n; i++) {
+const makeBoard = function makeBoard(n) {
+  const board = [];
+  for (let i = 0; i < n; i += 1) {
     board.push([]);
-    for (var j = 0; j < n; j++) {
+    for (let j = 0; j < n; j += 1) {
       board[i].push(false);
     }
   }
-  board.togglePiece = function(i, j) {
+  board.togglePiece = function togglePiece(i, j) {
     this[i][j] = !this[i][j];
   };
-  board.hasBeenVisited = function(i, j) {
+  board.hasBeenVisited = function hasBeenVisited(i, j) {
     return !!this[i][j];
   };
   return board;
 };
 
-var robotPaths = function(n, board, i, j) {
+/* Whiteboarding
+ * O: Number of possibilities
+ * I: N: board size, board: a Board class object, i: Robot starting column (from left), j: Robot starting row (from top)
+ * C: None
+ * E: None
+
+ * Approach
+ * Search Tree
+ */
+const robotPaths = function robotPaths(n, board, i, j) {
+  let pathCount = 0;
+
+  // Termination case
+  if (n <= 0) {
+    return 0;
+  }
+
+  if (!board.hasBeenVisited(i, j)) {
+    board.togglePiece(i, j);
+  }
+
+  // Try all combos
+  pathCount += robotPaths(n - 1, board, i + 1, j);
+
+  return pathCount;
 };
 
