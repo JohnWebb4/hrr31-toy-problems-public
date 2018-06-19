132597326ae8680b9ab3d57335a10d5c43760c6b
JohnWebb4 <John.Webb94@gmail.com>
2018-06-19 09:44:09 -0700

    Hide tests for robot paths

--- robotPaths/robotPathsSpec.js
+++ robotPaths/robotPathsSpec.js
@@ -1,5 +1,25 @@
-describe('robotPaths', () => {
+xdescribe('robotPaths', () => {
+  let board;
+  let n;
+  let i;
+  let j;
+
+  beforeEach(() => {
+    n = 5;
+    i = 0;
+    j = 0;
+    board = makeBoard(n);
+  });
+
   it('should be a function', () => {
     expect(robotPaths).to.be.a('function');
+  });
+
+  it('should return a number', () => {
+    expect(robotPaths(n, board, i, j)).to.be.a('number');
+  });
+
+  it('should return number of paths', () => {
+    expect(robotPaths(n, board, i, j)).to.equal(undefined);
   });
 });
