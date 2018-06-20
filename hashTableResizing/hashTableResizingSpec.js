ea5407c48f51f526d6d4a4efaad7b678472080f9
JohnWebb4 <John.Webb94@gmail.com>
2018-06-20 10:51:31 -0700

    Lint resizing hash table

--- hashTableResizing/hashTableResizingSpec.js
+++ hashTableResizing/hashTableResizingSpec.js
@@ -1,31 +1,36 @@
-describe('hashTableResizing', function() {
-  it('should be a function', function() {
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+
+const makeHashTable = require('./hashTableResizing');
+
+describe('hashTableResizing', () => {
+  it('should be a function', () => {
     expect(makeHashTable).to.be.a('function');
   });
 
-  it('should resize', function() {
+  it('should resize', () => {
     // if your hashtable isn't resizing, its going to start running more
     // and more slowly with a large number of inserts and retrievals.
-    var hashTable = makeHashTable();
-    var n = 10;
-    var id = 0;
-    var diffs = [];
-    var endTime;
-    var startTime;
-    for (var i = 0; i <= n; i++) {
+    const hashTable = makeHashTable();
+    const n = 10;
+    let id = 0;
+    const diffs = [];
+    let endTime;
+    let startTime;
+    for (let i = 0; i <= n; i += 1) {
       startTime = new Date();
-      for (var j = 0; j < 1000; j++) {
-        hashTable.insert('userid:' + id++, 'Syd Mead');
+      for (let j = 0; j < 1000; j += 1) {
+        hashTable.insert(`userid:${id += 1}`, 'Syd Mead');
       }
-      for (j = 0; j < 100; j++) {
-        hashTable.retrieve('userid:' + Math.floor(Math.random() * i));
+      for (let j = 0; j < 100; j += 1) {
+        hashTable.retrieve(`userid:${Math.floor(Math.random() * i)}`);
       }
       endTime = new Date();
       diffs.push(endTime - startTime);
     }
-    var sum = function sum(arr) {
-      var total = 0;
-      for (var i = 0; i < arr.length; i++) {
+    const sum = function sum(arr) {
+      let total = 0;
+      for (let i = 0; i < arr.length; i += 1) {
         total += arr[i];
       }
       return total;
@@ -33,37 +38,37 @@ describe('hashTableResizing', function() {
     // we should expect the first iteration to take up ruffly 1 / n
     // of the total time. we give it some wiggle room by letting it be as
     // low as a 1 / (n*2) of the total duration.
-    var ratio = diffs[0] / sum(diffs);
+    const ratio = diffs[0] / sum(diffs);
     expect(ratio).to.be.above(1 / (n * 2));
   });
 
-  it('should return stored values', function() {
-    var hashTable = makeHashTable();
+  it('should return stored values', () => {
+    const hashTable = makeHashTable();
     hashTable.insert('William Shatner\'s most well known role', 'Captain Kirk');
-    var value = hashTable.retrieve('William Shatner\'s most well known role');
+    const value = hashTable.retrieve('William Shatner\'s most well known role');
     expect(value).to.be.a('string');
     expect(value).to.equal('Captain Kirk');
   });
 
-  it('should return undefined for incorrect key', function() {
-    var hashTable = makeHashTable();
-    expect(hashTable.retrieve('echo?')).to.be.undefined;
+  it('should return undefined for incorrect key', () => {
+    const hashTable = makeHashTable();
+    expect(hashTable.retrieve('echo?')).to.equal(undefined);
   });
 
-  it('should update values', function() {
-    var hashTable = makeHashTable();
+  it('should update values', () => {
+    const hashTable = makeHashTable();
     hashTable.insert('Tarantino\'s best movie', 'Jackie Brown');
     hashTable.insert('Tarantino\'s best movie', 'Pulp Fiction');
-    var value = hashTable.retrieve('Tarantino\'s best movie');
-    expect(value).to.not.be.null;
+    const value = hashTable.retrieve('Tarantino\'s best movie');
+    expect(value).to.not.equal(null);
     expect(value).to.equal('Pulp Fiction');
   });
 
-  it('should remove values', function() {
-    var hashTable = makeHashTable();
+  it('should remove values', () => {
+    const hashTable = makeHashTable();
     hashTable.insert('Spielberg\'s best movie', 'Jaws');
     hashTable.remove('Spielberg\'s best movie');
-    var value = hashTable.retrieve('Spielberg\'s best movie');
-    expect(value).to.be.undefined;
+    const value = hashTable.retrieve('Spielberg\'s best movie');
+    expect(value).to.equal(undefined);
   });
 });
