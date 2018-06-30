abc2d3988bbed07b7f270cec0d5358dbdeb63a6c
JohnWebb4 <John.Webb94@gmail.com>
2018-06-30 09:41:32 -0700

    Test robot paths handles 0x0 and 1x1 boards

--- robotPaths/robotPathsSpec.js
+++ robotPaths/robotPathsSpec.js
@@ -1,30 +1,36 @@
-// import { beforeEach, describe, it } from 'mocha';
-// import { expect } from 'chai';
+import { beforeEach, describe, it } from 'mocha';
+import { expect } from 'chai';
 
-// import { makeBoard, robotPaths } from './robotPaths';
+import { makeBoard, robotPaths } from './robotPaths';
 
-// describe('robotPaths', () => {
-//   let board;
-//   let n;
-//   let i;
-//   let j;
+describe('robotPaths', () => {
+  let board;
+  let n;
+  let i;
+  let j;
 
-//   beforeEach(() => {
-//     n = 5;
-//     i = 0;
-//     j = 0;
-//     board = makeBoard(n);
-//   });
+  beforeEach(() => {
+    n = 5;
+    i = 0;
+    j = 0;
+    board = makeBoard(n);
+  });
 
-//   it('should be a function', () => {
-//     expect(robotPaths).to.be.a('function');
-//   });
+  it('should be a function', () => {
+    expect(robotPaths).to.be.a('function');
+  });
 
-//   it('should return a number', () => {
-//     expect(robotPaths(n, board, i, j)).to.be.a('number');
-//   });
+  it('should return a number', () => {
+    expect(robotPaths(n, board, i, j)).to.be.a('number');
+  });
 
-//   it('should return number of paths', () => {
-//     expect(robotPaths(n, board, i, j)).to.equal(undefined);
-//   });
-// });
+  it('should return zero for 0x0 board', () => {
+    n = 0;
+    expect(robotPaths(n, makeBoard(n), i, j)).to.equal(0);
+  });
+
+  it('should return one for 1x1 board', () => {
+    n = 1;
+    expect(robotPaths(n, makeBoard(n), i, j)).to.equal(1);
+  });
+});
