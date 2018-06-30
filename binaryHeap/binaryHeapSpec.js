8eb4cd263a106240abc68068f93a47cfe80b18ea
JohnWebb4 <John.Webb94@gmail.com>
2018-06-29 23:23:40 -0700

    Test binary heap inserts root node

--- binaryHeap/binaryHeapSpec.js
+++ binaryHeap/binaryHeapSpec.js
@@ -1,4 +1,21 @@
-import { describe } from 'mocha';
+import { beforeEach, describe, it } from 'mocha';
+import { expect } from 'chai';
+
+import BinaryHeap from './binaryHeap';
 
 describe('binaryHeap', () => {
+  let binaryHeap;
+
+  beforeEach(() => {
+    binaryHeap = new BinaryHeap();
+  });
+
+  it('should get null root for empty heap', () => {
+    expect(binaryHeap.getRoot()).to.equal(undefined);
+  });
+
+  it('should insert node at root', () => {
+    expect(binaryHeap.insert(1)).to.equal(1);
+    expect(binaryHeap.getRoot()).to.equal(1);
+  });
 });
