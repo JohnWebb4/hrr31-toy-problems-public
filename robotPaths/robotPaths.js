5f1acada5fc367ad090fe4193ffc757d53277965
JohnWebb4 <John.Webb94@gmail.com>
2018-06-30 09:50:49 -0700

    Calculate all robot paths for a board

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -9,8 +9,11 @@
  *
  */
 
-// A Board class will be useful
-
+/**
+ * Board for calculating robot paths
+ * @constructs Board
+ * @param {number} n Size of the board (nxn)
+ */
 const makeBoard = function makeBoard(n) {
   const board = [];
   for (let i = 0; i < n; i += 1) {
@@ -19,41 +22,68 @@ const makeBoard = function makeBoard(n) {
       board[i].push(false);
     }
   }
+
+  /**
+   * Toggle piece on board
+   * @function togglePiece
+   * @param {number} i index of x axis
+   * @param {number} j index of y axis
+   */
   board.togglePiece = function togglePiece(i, j) {
     this[i][j] = !this[i][j];
   };
+
+  /**
+   * Determine if a place on board has been visited
+   * @function hasBeenVisited
+   * @param {number} i index of x axis
+   * @param {number} j index of y axis
+   * @returns {boolean} If space has been visited
+   */
   board.hasBeenVisited = function hasBeenVisited(i, j) {
     return !!this[i][j];
   };
   return board;
 };
 
-/* Whiteboarding
- * O: Number of possibilities
- * I: N: board size,
- * board: a Board class object,
- * i: Robot starting column (from left),
- * j: Robot starting row (from top)
- * C: None
- * E: None
-
- * Approach
- * Search Tree
+/**
+ * Determine if place on board has been visited
+ * @function robotPaths
+ * @param {number} n board size,
+ * @param {Board} board a Board class object,
+ * @param {number} i Robot starting column (from left),
+ * @param {number} j Robot starting row (from top)
+ * @return {number} Number of possibilities
  */
 const robotPaths = function robotPaths(n, board, i, j) {
-  let pathCount = 0;
-
   // Termination case
   if (n <= 0) {
     return 0;
   }
 
-  if (!board.hasBeenVisited(i, j)) {
-    board.togglePiece(i, j);
+  // If at end of board
+  if (i === n - 1 && j === n - 1) {
+    return 1;
   }
 
-  // Try all combos
-  pathCount += robotPaths(n - 1, board, i + 1, j);
+  let pathCount = 0;
+
+  board.togglePiece(i, j);
+
+  // Calculate next paths
+  const nextPath = [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1]];
+
+  nextPath.forEach(([nextI, nextJ]) => {
+    // If on board
+    if (nextI >= 0 && nextI < n && nextJ >= 0 && nextJ < n) {
+      // Is has not been visited
+      if (!board.hasBeenVisited(nextI, nextJ)) {
+        pathCount += robotPaths(n, board, nextI, nextJ);
+      }
+    }
+  });
+
+  board.togglePiece(i, j);
 
   return pathCount;
 };
