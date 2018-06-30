88c2c35354d4e3559a561f8710008da6a1eef055
JohnWebb4 <John.Webb94@gmail.com>
2018-06-30 09:47:58 -0700

    Test robot paths handles 2x2 and 3x3 boards

--- robotPaths/robotPathsSpec.js
+++ robotPaths/robotPathsSpec.js
@@ -33,4 +33,14 @@ describe('robotPaths', () => {
     n = 1;
     expect(robotPaths(n, makeBoard(n), i, j)).to.equal(1);
   });
+
+  it('should return two 2x2 board', () => {
+    n = 2;
+    expect(robotPaths(n, makeBoard(n), i, j)).to.equal(2);
+  });
+
+  it('should return eight for 3x3 board', () => {
+    n = 3;
+    expect(robotPaths(n, makeBoard(n), i, j)).to.equal(12);
+  });
 });
