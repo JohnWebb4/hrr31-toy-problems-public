38d760b7ce83bb9f3d7cb7054ed72561396f3b99
JohnWebb4 <John.Webb94@gmail.com>
2018-07-02 11:26:26 -0700

    Convert rest args to es5

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,10 @@ module.exports = {
         "no-param-reassign": 0,
         "no-underscore-dangle": 0,
         "prefer-rest-params": 0,
+<<<<<<< 12292fd8cf1fce954fcbe0a0c81776e7c2962391
         "prefer-template": 0,
+=======
+>>>>>>> Convert rest args to es5
     },
     "env": {
       "browser": true,
