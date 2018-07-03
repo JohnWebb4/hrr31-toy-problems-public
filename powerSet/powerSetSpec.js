9730512930e16a74b73520ec6cf4300bb7ae0fdc
JohnWebb4 <John.Webb94@gmail.com>
2018-07-03 10:59:02 -0700

    Test power set of "abc"

--- powerSet/powerSetSpec.js
+++ powerSet/powerSetSpec.js
@@ -11,4 +11,11 @@ describe('powerSet', () => {
   it('should return one possiblity for empty string', () => {
     expect(powerSet('')).to.eql(['']);
   });
+
+  it('should get power set of "abc"', () => {
+    const string = 'abc';
+    const result = ['', 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc'];
+
+    expect(powerSet(string)).to.eql(result);
+  });
 });
