650b178330d3a5be4bc6c1375ca85c2d9337e982
JohnWebb4 <John.Webb94@gmail.com>
2018-07-08 22:27:57 -0700

    Test can bubble sort large arrays

--- bubbleSort/bubbleSortSpec.js
+++ bubbleSort/bubbleSortSpec.js
@@ -21,4 +21,17 @@ describe('bubble sort', () => {
   it('should sort numbers with decimals', () => {
     expect(bubbleSort([1.2, -1.5, -2.1, -3.999, -3.998])).to.eql([-3.999, -3.998, -2.1, -1.5, 1.2]);
   });
+
+  it('should sort large arrays', () => {
+    // Creat array
+    const arr = [];
+    const result = [];
+    // Add values in descending order
+    for (let value = 15000; value > 0; value -= 1) {
+      arr.push(value);
+      result.unshift(value);
+    }
+    // Expect can sort quickly
+    expect(bubbleSort(arr)).to.eql(result);
+  }).timeout(1000);
 });
