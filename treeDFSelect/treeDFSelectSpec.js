ec190ccb7eeb8ade106eea4d68c0499c1ae2a6f5
JohnWebb4 <John.Webb94@gmail.com>
2018-07-08 23:23:50 -0700

    Test tree depth first filter returns an array

--- treeDFSelect/treeDFSelectSpec.js
+++ treeDFSelect/treeDFSelectSpec.js
@@ -6,5 +6,12 @@ import Tree from './treeDFSelect';
 describe('treeDFSelect', () => {
   it('should be a psuedoclassical class', () => {
     expect(Tree).to.be.a('function');
+    expect(Tree.prototype.DFSelect).to.be.a('function');
+  });
+
+  it('should return an array', () => {
+    // Define tree
+    const tree = new Tree(1);
+    expect(tree.DFSelect(value => (value === 1 ? 1 : 0))).to.be.an('array');
   });
 });
