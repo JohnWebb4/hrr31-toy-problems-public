45daab6913a4d8d4d0160dd158242d25da063501
JohnWebb4 <John.Webb94@gmail.com>
2018-07-08 22:45:43 -0700

    Test common characters returns a string

--- commonCharacters/commonCharactersSpec.js
+++ commonCharacters/commonCharactersSpec.js
@@ -7,4 +7,8 @@ describe('commonCharacters', () => {
   it('should be a function', () => {
     expect(commonCharacters).to.be.a('function');
   });
+
+  it('should return a string', () => {
+    expect(commonCharacters('hi', 'hi')).to.be.a('string');
+  });
 });
