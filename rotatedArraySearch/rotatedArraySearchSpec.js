3409bbb6937a86ae82ee222b1ae309face9413c5
JohnWebb4 <John.Webb94@gmail.com>
2018-07-09 00:20:04 -0700

    Test returns null if not in rotated array

--- rotatedArraySearch/rotatedArraySearchSpec.js
+++ rotatedArraySearch/rotatedArraySearchSpec.js
@@ -11,4 +11,8 @@ describe('rotatedArraySearch', () => {
   it('should return a number', () => {
     expect(rotatedArraySearch([1, 2], 2)).to.equal(1);
   });
+
+  it('should return null if not in array', () => {
+    expect(rotatedArraySearch([1, 2], 3)).to.equal(null);
+  });
 });
