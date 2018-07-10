0e52fe34fa026078e11c4453a2acab1181de820f
JohnWebb4 <John.Webb94@gmail.com>
2018-07-10 09:48:15 -0700

    Test can create range

--- rangeClass/rangeClassSpec.js
+++ rangeClass/rangeClassSpec.js
@@ -10,4 +10,18 @@ describe('Range', () => {
     expect(Range.prototype.includes).to.be.a('function');
     expect(Range.prototype.size).to.be.a('function');
   });
+
+  it('should create a range', () => {
+    // Check default range
+    const defaultRange = new Range(1);
+    expect(defaultRange.start).to.equal(1);
+    expect(defaultRange.end).to.equal(1);
+    expect(defaultRange.step).to.equal(1);
+
+    // Check range
+    const range = new Range(2, 6, 2);
+    expect(range.start).to.equal(2);
+    expect(range.end).to.equal(6);
+    expect(range.step).to.equal(2);
+  });
 });
