e6d71aaad2cf44444b85e526d49ccbb9e772fa70
JohnWebb4 <John.Webb94@gmail.com>
2018-07-13 09:31:10 -0700

    Test returns a pair of indices

--- longestRun/longestRunSpec.js
+++ longestRun/longestRunSpec.js
@@ -7,4 +7,12 @@ describe('longestRun', () => {
   it('should be a function', () => {
     expect(longestRun).to.be.a('function');
   });
+
+  it('should return an pair of indices', () => {
+    const runIndexies = longestRun('hello');
+    expect(runIndexies).to.be.an('array');
+    expect(runIndexies.length).to.equal(2);
+    expect(runIndexies[0]).to.be.a('number');
+    expect(runIndexies[1]).to.be.a('number');
+  });
 });
