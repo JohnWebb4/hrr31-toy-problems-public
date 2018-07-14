499f2b979f5e7d666fc6c6f282028cbc25ddec56
JohnWebb4 <John.Webb94@gmail.com>
2018-07-14 09:45:44 -0700

    Test merge sort returns an array

--- mergeSort/mergeSortSpec.js
+++ mergeSort/mergeSortSpec.js
@@ -7,4 +7,8 @@ describe('mergeSort', () => {
   it('should be a function', () => {
     expect(mergeSort).to.be.a('function');
   });
+
+  it('should return an array', () => {
+    expect(mergeSort([1, 2])).to.be.an('array');
+  });
 });
