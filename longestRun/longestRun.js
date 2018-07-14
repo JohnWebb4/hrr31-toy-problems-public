1789ccb1bc412428440a68477ec2972b15cdb5fe
JohnWebb4 <John.Webb94@gmail.com>
2018-07-14 09:16:37 -0700

    Get the longest run of characters in a string

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Write a function that, given a string, Finds the longest run of identical
  * characters and returns an array containing the start and end indices of
  * that run. If there are two runs of equal length, return the first one.
@@ -13,15 +13,71 @@
  * inputs well.
  */
 
+/**
+ * Get the longest run of duplicate
+ * characters in a string
+ * @param {string} string String to get longest run of
+ * @returns {number[] | null} Pair of start and end indices
+ */
 const longestRun = function longestRun(string) {
-  // TODO: Your code here!
+  if (typeof string !== 'string' || string === '') {
+    // Empty string or not a string
+    // Return null
+    return null;
+  }
+
+  // Defined longest run values
+  let longestRunStart = 0;
+  let longestRunEnd = 0;
+  let longestRunCount = 0;
+
+  // Define current run values
+  let currentRunCount = 0;
+  let currentRunChar = '';
+
+  const updateLongest = (char, index) => {
+    if (currentRunCount > longestRunCount) {
+      // Update longest run
+      longestRunStart = (index - currentRunCount);
+      longestRunEnd = index - 1;
+      longestRunCount = currentRunCount;
+    }
+
+    // Update current run
+    currentRunCount = 1;
+    currentRunChar = char;
+  };
+
+  string.split('').forEach((char, index) => {
+    // Compare each character for a run
+    if (char === currentRunChar) {
+      // Still in run increment counter
+      currentRunCount += 1;
+      return;
+    }
+
+    // End of run
+    // Update longest run
+    updateLongest(char, index);
+  });
+
+  // Update for last run
+  updateLongest('', string.length);
+
+  // Return longest run
+  return [longestRunStart, longestRunEnd];
 };
 
 // If you need a random string generator, use this!
 // (you wont need this function for your solution but it may help with testing)
+/**
+ * Generates a random string of alphabetical characters
+ * @param {number} len Length of string
+ * @returns {string} Random alphabetical string
+ */
 const randomString = function randomString(len) {
   let text = '';
-  let possible = 'abcdefghijklmnopqrstuvwxyz';
+  const possible = 'abcdefghijklmnopqrstuvwxyz';
 
   for (let i = 0; i < len; i += 1) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -31,5 +87,8 @@ const randomString = function randomString(len) {
 };
 
 if (window.DEBUG) {
-  module.exports = longestRun;
+  module.exports = {
+    longestRun,
+    randomString,
+  };
 }
