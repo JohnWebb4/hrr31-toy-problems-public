fdcdc549518f531ff907e551c8cae655f564d15d
JohnWebb4 <John.Webb94@gmail.com>
2018-07-15 15:36:08 -0700

    Test deep equals objects

--- deepEquality/deepEqualitySpec.js
+++ deepEquality/deepEqualitySpec.js
@@ -11,4 +11,28 @@ describe('deepEquals', () => {
   it('should return a boolean', () => {
     expect(deepEquals({}, {})).to.be.a('boolean');
   });
+
+  it('should return deep equals', () => {
+    const a = {
+      a: 1, b: true, c: undefined, d: null,
+    };
+    const aSame = {
+      a: 1, b: true, c: undefined, d: null,
+    };
+
+    // Test deep equal objects
+    expect(deepEquals(a, aSame)).to.equal(true);
+
+    const aExtended = Object.assign({}, a);
+    aExtended.newThing = 'something';
+
+    // Test extended object
+    expect(deepEquals(a, aExtended)).to.equal(false);
+
+    const a3 = Object.assign({}, a);
+    a3.a = 3;
+
+    // Test object with different primative values
+    expect(deepEquals(a, a3)).to.equal(false);
+  });
 });
