5259802889fc6ac7474af10385427dc64ca911dd
JohnWebb4 <John.Webb94@gmail.com>
2018-07-15 13:29:41 -0700

    Test largest product of three returns a number

--- largestProductOfThree/largestProductOfThreeSpec.js
+++ largestProductOfThree/largestProductOfThreeSpec.js
@@ -7,4 +7,8 @@ describe('largestProductOfThree', () => {
   it('should be a function', () => {
     expect(largestProductOfThree).to.be.a('function');
   });
+
+  it('should return a number', () => {
+    expect(largestProductOfThree([1, 2, 3])).to.be.a('number');
+  });
 });
