36b5d11e7868b54ee59a625143af45fb91b26a98
JohnWebb4 <John.Webb94@gmail.com>
2018-07-17 09:44:13 -0700

    Export fraction converter and lint

--- fractionConverter/fractionConverter.js
+++ fractionConverter/fractionConverter.js
@@ -1,17 +1,27 @@
-/**
- * Write a function that takes a number as its argument and 
+/*
+ * Write a function that takes a number as its argument and
  * returns a string that represents that number's simplified fraction.
  *
  * Example: toFraction(0.5) === '1/2'
- * 
+ *
  * Whole numbers and mixed fractions should be returned as irregular fractions
- * 
+ *
  * Example: toFraction(3.0) === '3/1'
- * 
+ *
  * Example: toFraction(2.5) === '5/2'
  *
  */
 
-var toFraction = function(number) {
+/**
+ * Convert decimal to fraction
+ * @param {number} number Decimal number to convert
+ * @returns {string} Fraction equivalent
+ */
+const toFraction = function toFraction(number) {
   // Your code here
 };
+
+if (window.DEBUG) {
+  // If debugging, export to fractions
+  module.exports = toFraction;
+}
