fc94f888a1494dd01414fc7eff54af638212116f
JohnWebb4 <John.Webb94@gmail.com>
2018-07-17 10:14:03 -0700

    Test fraction converter converts fractions

--- fractionConverter/fractionConverterSpec.js
+++ fractionConverter/fractionConverterSpec.js
@@ -11,4 +11,10 @@ describe('fractionConverter', () => {
   it('should return a string', () => {
     expect(toFraction(1.0)).to.be.a('string');
   });
+
+  it('should return a fraction', () => {
+    expect(toFraction(0.5)).to.equal('1/2');
+    expect(toFraction(0.25)).to.equal('1/4');
+    expect(toFraction(0.2)).to.equal('1/5');
+  });
 });
