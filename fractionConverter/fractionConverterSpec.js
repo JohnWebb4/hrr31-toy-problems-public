1aafe118742ffced796af5e46e710fa708ca8b0d
JohnWebb4 <John.Webb94@gmail.com>
2018-07-17 09:45:34 -0700

    Test fraction converter returns a string

--- fractionConverter/fractionConverterSpec.js
+++ fractionConverter/fractionConverterSpec.js
@@ -1,10 +1,14 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
-import toFracton from './fractionConverter';
+import toFraction from './fractionConverter';
 
 describe('fractionConverter', () => {
   it('should be a function', () => {
-    expect(toFracton).to.be.a('function');
+    expect(toFraction).to.be.a('function');
+  });
+
+  it('should return a string', () => {
+    expect(toFraction(1.0)).to.be.a('string');
   });
 });
