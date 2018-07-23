77474a6d9180719bd4b58f42f2d200a705d41c2e
JohnWebb4 <John.Webb94@gmail.com>
2018-07-23 16:51:05 -0700

    Test converts mixed, negative, and large fractions

--- fractionConverter/fractionConverterSpec.js
+++ fractionConverter/fractionConverterSpec.js
@@ -27,4 +27,40 @@ describe('fractionConverter', () => {
     expect(toFraction(1.5)).to.equal('3/2');
     expect(toFraction(2.5)).to.equal('5/2');
   });
+
+  it('should return the correct fraction 1.0', () => {
+    expect(toFraction(1.0)).to.equal('1/1');
+  });
+
+  it('should return the correct faction for 0.25', () => {
+    expect(toFraction(0.25)).to.equal('1/4');
+  });
+
+  it('should return the correct faction for 0.88', () => {
+    expect(toFraction(0.88)).to.equal('22/25');
+  });
+
+  it('should return the correct faction for 0.88', () => {
+    expect(toFraction(.88)).to.equal('22/25');
+  });
+
+  it('should return the correct faction for 0.253213', () => {
+    expect(toFraction(0.253213)).to.equal('253213/1000000');
+  });
+
+  it('should return the correct faction for 1.75', () => {
+    expect(toFraction(1.75)).to.equal('7/4');
+  });
+
+  it('should return the correct fraction for 0.0', () => {
+    expect(toFraction(0)).to.equal('0/1');
+  });
+
+  it('should return the correct faction for 82', () => {
+    expect(toFraction(82.0)).to.equal('82/1');
+  });
+
+  it('should work for negative numbers', () => {
+    expect(toFraction(-1.75)).to.equal('-7/4');
+  });
 });
