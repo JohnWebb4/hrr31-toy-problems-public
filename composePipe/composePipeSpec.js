f7fc9d1e18150985913af23312c20c65d8163780
JohnWebb4 <John.Webb94@gmail.com>
2018-07-25 09:51:08 -0700

    Test composes functions

--- composePipe/composePipeSpec.js
+++ composePipe/composePipeSpec.js
@@ -15,4 +15,23 @@ describe('composePipe', () => {
     expect(compose(testFunc)).to.be.a('function');
     expect(pipe(testFunc)).to.be.a('function');
   });
+
+  it('should compose functions', () => {
+    // Create test functions
+    const addThree = num => num + 3;
+    const multiplyFive = num => num * 5;
+    const divideFour = num => num / 4;
+
+    // Test empty compose
+    expect(compose()).to.equal(undefined);
+
+    // Test compose with arguments
+    expect(compose(addThree)(4)).to.equal(7);
+    expect(compose(multiplyFive)(5)).to.equal(25);
+
+    expect(compose(addThree, divideFour)(4)).to.equal(4);
+    expect(compose(multiplyFive, addThree)(2)).to.equal(25);
+
+    expect(compose(multiplyFive, divideFour, addThree)(1)).to.equal(5);
+  });
 });
