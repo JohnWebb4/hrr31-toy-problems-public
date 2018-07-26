285505ccbba0e61e07bfb780e8999a32ab02cb00
JohnWebb4 <John.Webb94@gmail.com>
2018-07-26 10:49:51 -0700

    Test async map passes array to callback

--- asyncMap/asyncMapSpec.js
+++ asyncMap/asyncMapSpec.js
@@ -7,4 +7,8 @@ describe('asyncMap', () => {
   it('should be a function', () => {
     expect(asyncMap).to.be.a('function');
   });
+
+  it('should pass array to callback', () => {
+    asyncMap([], results => expect(results).to.be.an('array'));
+  });
 });
